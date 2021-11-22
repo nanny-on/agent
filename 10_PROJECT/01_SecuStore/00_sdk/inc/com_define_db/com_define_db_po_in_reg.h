c0ce19cfb5bc3e7bff45f812819948d0a2b11782
jhrang <driverdev@selftrust.co.kr>
2021-11-22 14:37:46 +0900

    fix(license) : update to license at *.cpp *.h files

--- 10_PROJECT/01_SecuStore/00_sdk/inc/com_define_db/com_define_db_po_in_reg.h
+++ 10_PROJECT/01_SecuStore/00_sdk/inc/com_define_db/com_define_db_po_in_reg.h
@@ -4,7 +4,9 @@
  * This program is free software; you can redistribute it and/or 
  * modify it under the terms of the GNU General Public 
  * License as published by the Free Software Foundation; either 
- * version 3 of the License, or any later version. 
+ * version 3 of the License, or any later version, with the additional
+ * special exception to link portions of this program with the OpenSSL
+ * library. See LICENSE for more details. 
  * 
  * This program is distributed in the hope that it will be useful, 
  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
@@ -20,7 +22,9 @@
  * This program is free software; you can redistribute it and/or 
  * modify it under the terms of the GNU General Public 
  * License as published by the Free Software Foundation; either 
- * version 3 of the License, or any later version. 
+ * version 3 of the License, or any later version, with the additional
+ * special exception to link portions of this program with the OpenSSL
+ * library. See LICENSE for more details. 
  * 
  * This program is distributed in the hope that it will be useful, 
  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
